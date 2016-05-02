5a41eded83005b6b0f93f1f8b83366e09c644caf
moiseevanton <athidif94@gmail.com>
2016-05-02 19:17:08 -0400

    Initial Commit

---
+++ Word Effects/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Word Effects
+//
+//  Created by Anton Moiseev on 2016-05-02.
+//  Copyright © 2016 Anton Moiseev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
